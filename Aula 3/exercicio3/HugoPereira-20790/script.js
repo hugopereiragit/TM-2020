126b71cb752a8eb298fa08ba9a867cd3968e3ba9
EvocativeAxisPt <61696448+EvocativeAxisPt@users.noreply.github.com>
2020-03-09 14:51:33 +0000

    HugoPereira-20790
    Aula3Exe3

---
+++ Aula 3/exercicio3/HugoPereira-20790/script.js
@@ -0,0 +1,34 @@
+function changeColors(){
+ var titulosVar = document.getElementsByClassName("titulo");
+ for (let titulo of titulosVar){
+     // o titulo vai ser uma iteracao do titulosVar
+     titulo.style.color="red";
+ }
+}
+function getInfo(){
+    var link = document.getElementById("idLink");
+    var lista = document.getElementById("info");
+    lista.innerHTML=
+        "ID: "+ link.id + "<br>" +
+        "Target: "+ link.target + "<br>" +
+        "Type: "+ link.type + "<br>" +
+        "Href: "+ link.href + "<br>"  ;
+}
+
+function setClick() {
+    var cells = document.getElementsByTagName("td");
+    console.log(cells);
+    for (var i = 0; i < cells.length; i++) {
+    cells[i].onclick= function (){
+        this.innerText = clickId();
+        this.style.backgroundColor = "green";
+    }
+    }
+}
+
+function clickId(){
+  return prompt("novo Valor");
+}
+
+setClick();
+//setTimeout(()=>setClick(), 0)
