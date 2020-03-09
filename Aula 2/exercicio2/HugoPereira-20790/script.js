2d4555cbd33b36e75831b4fbcceb0ea34045576d
EvocativeAxisPt <61696448+EvocativeAxisPt@users.noreply.github.com>
2020-03-09 13:43:41 +0000

    Revert "aula2exe2"
    This reverts commit fb9c2af542341d6b8ff62308c987136a3e4f6697.

--- Aula 2/exercicio2/HugoPereira-20790/script.js
+++
@@ -1,9 +0,0 @@
-var arrayNumbers=[];
-function adicionarNumero() {
-    var number = document.getElementById("newNumbers").value;
-    arrayNumbers.push(parseInt(number));
-    console.log(arrayNumbers);
-}
-function calcular() {
-    alert(Math.max.apply(Math,arrayNumbers))
-}
