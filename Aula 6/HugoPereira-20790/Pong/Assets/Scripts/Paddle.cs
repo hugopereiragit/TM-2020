bd6fb6343776cdaf02cdc66aca97fee98830cffd
EvocativeAxisPt <61696448+EvocativeAxisPt@users.noreply.github.com>
2020-03-26 14:55:33 +0000

    HugoPereira 20790
    Exercicio pong unity

---
+++ Aula 6/HugoPereira-20790/Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    public Vector3 startPosition;
+
+    private float movement;
+
+    void Start()
+    {
+        startPosition = transform.position; // guarda primeira posicao
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Vertical"); // Deafult w/s movement
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed); // velocidade da paddle
+        
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero; // reset velocidade
+        transform.position = startPosition;
+    }
+}
---
+++ Aula 6/HugoPereira-20790/Pong/Assets/Scripts/manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class manager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Score UI")]
+    public GameObject player1Text;
+    private int playerScore;
+
+    public void Player1Scored()
+    {
+        playerScore++;
+        player1Text.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+        ball.GetComponent<Ball>().increaseSpeed();
+    }
+
+    public void Reset()
+    {
+        playerScore = 0;
+        player1Text.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+    }
+
+}
